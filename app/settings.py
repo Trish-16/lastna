068745ffed0ba3595a7e7c02fa1c59dd32d9671c
Trish-16 <125871143+Trish-16@users.noreply.github.com>
2024-05-27 16:22:53 +0800

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,8 +1,21 @@
-import os
-
-IS_PRODUCTION = os.environ.get('IS_PRODUCTION')
-
-if IS_PRODUCTION:
-    from .conf.production.settings import *
-else:
-    from .conf.development.settings import *
+//devproject/settings.py
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myapp', #add myapp
+]
+    
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
