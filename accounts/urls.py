15976efea08ae04879168cb4ab7a932d2b2bab8f
Trish-16 <125871143+Trish-16@users.noreply.github.com>
2024-05-27 16:27:47 +0800

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,31 +1,13 @@
+//devproject/urls.py
+from django.contrib import admin
 from django.urls import path
-
-from .views import (
-    LogInView, ResendActivationCodeView, RemindUsernameView, SignUpView, ActivateView, LogOutView,
-    ChangeEmailView, ChangeEmailActivateView, ChangeProfileView, ChangePasswordView,
-    RestorePasswordView, RestorePasswordDoneView, RestorePasswordConfirmView, LogOutConfirmView,
-)
-
-app_name = 'accounts'
-
+from myapp import views
+ 
 urlpatterns = [
-    path('log-in/', LogInView.as_view(), name='log_in'),
-    path('log-out/confirm/', LogOutConfirmView.as_view(), name='log_out_confirm'),
-    path('log-out/', LogOutView.as_view(), name='log_out'),
-
-    path('resend/activation-code/', ResendActivationCodeView.as_view(), name='resend_activation_code'),
-
-    path('sign-up/', SignUpView.as_view(), name='sign_up'),
-    path('activate/<code>/', ActivateView.as_view(), name='activate'),
-
-    path('restore/password/', RestorePasswordView.as_view(), name='restore_password'),
-    path('restore/password/done/', RestorePasswordDoneView.as_view(), name='restore_password_done'),
-    path('restore/<uidb64>/<token>/', RestorePasswordConfirmView.as_view(), name='restore_password_confirm'),
-
-    path('remind/username/', RemindUsernameView.as_view(), name='remind_username'),
-
-    path('change/profile/', ChangeProfileView.as_view(), name='change_profile'),
-    path('change/password/', ChangePasswordView.as_view(), name='change_password'),
-    path('change/email/', ChangeEmailView.as_view(), name='change_email'),
-    path('change/email/<code>/', ChangeEmailActivateView.as_view(), name='change_email_activation'),
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signin/',views.signin, name='signin'),
+    path('signout/',views.signout, name='signout'),
+    path('signup/',views.signup, name='signup'),
+    path('profile/',views.profile, name='profile'),
 ]
